91918295b496351d2b48298c8d80874405b36751
kubersharma001 <kubersharma001@gmail.com>
2022-10-12 12:41:07 +0000

    elish: Don't inherit updatable_apex.mk
    - prebuilt vendor devices must set this prop in /product to override vendor value:
    ro.apex.updatable=false
    
    This is because of the following reasons:
    - Many build scripts force disable it anyways by settings OVERRIDE_TARGET_FLATTEN_APEX env flag so why bother rendering differences in official and local builds [1][2].
    - Compressed apex causes issues with OTA on A only devices [3].
    - Compressed apex may cause issues on devices which do not meet kernel requirements [4].
    - Highly anticipated mainline modules update aka Play System updates is limited to OEMs/Pixels only [5], we don't do such things on AOSP; shipping Pixel prebuilt apex components feels somewhat unethical.
    
    Ref:
    [1]: https://github.com/lineageos-infra/build-config/blob/8e435ce7ff7c74097b3500e143380273e099aefb/android/build.sh#L9
    [2]: https://github.com/LineageOS/android_build/blob/lineage-18.1/target/product/updatable_apex.mk#L19
    [3]: RecoverySystemService: Failed to reserve space for compressed apex
    [4]: https://source.android.com/devices/tech/ota/apex#kernel-requirements
    [5]: "pushed to end-user devices, either by Google (using the Google Play Store infrastructure) or by the Android partner (using a partner-provided OTA mechanism)." as stated in https://source.android.com/devices/architecture/modular-system#architecture
    
    test: m, boots and ota.
    
    Change-Id: I5b5c2366398791100fa2222ef779ca4d90842211

--- device.mk
+++ device.mk
@@ -8,9 +8,6 @@ $(call inherit-product, vendor/xiaomi/elish/elish-vendor.mk)
 
 ELISH_PREBUILT := device/xiaomi/elish-prebuilt
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
